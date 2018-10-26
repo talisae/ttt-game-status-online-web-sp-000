60a4670dfa80a9d0404dd10d3c673ec45c650c34
Talisa Erazo <talisaerazo@gmail.com>
2018-10-26 05:09:56 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -49,7 +49,7 @@ def over?(board)
 end
 
 def winner(board)
-  if combo.all? {|index| board[index] == "X"}
+  if combo.any? {|index| board[index] == "X"}
     winner = "X"
   elsif combo.all? {|index| board[index] == "O"}
     winner = "O"
